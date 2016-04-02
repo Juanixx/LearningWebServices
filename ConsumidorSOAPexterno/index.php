37646af2ab6b57bb85bc16c60883561b8c01bc60
Juan Eduardo Calderón Servín <juanixgh@gmail.com>
2016-04-02 14:05:32 -0600

    Se muestra como consumir datos complejos de un web service externo

--- ConsumidorSOAPexterno/index.php
+++ ConsumidorSOAPexterno/index.php
@@ -9,11 +9,36 @@ $resultado=$client->call("GetCityForecastByZIP", array("ZIP"=>"90210"));
 
 /*Para imprimir los valores que se obtienen del Web Service, y para obtener valores 
 más concretos se observa el código fuente en el navegador*/
-print_r($resultado);
+//print_r($resultado);
 
 $ciudad=$resultado['GetCityForecastByZIPResult']['City'];
 $estado=$resultado['GetCityForecastByZIPResult']['State'];
 
 echo "<h1>Pronóstico del Tiempo (".$ciudad.", ".$estado.")</h1>";
+
+//Se va a empezar a procesar datos complejos de Web Services externos
+$pronosticos= $resultado['GetCityForecastByZIPResult']['ForecastResult']['Forecast'];
+
+foreach($pronosticos as $diaPronostico){
+	$fecha = $diaPronostico['Date'];
+	$descripcion= $diaPronostico['Desciption'];
+
+	$minima=$diaPronostico['Temperatures']['MorningLow'];
+	$maxima=$diaPronostico['Temperatures']['MorningLHigh'];
+
+	$fechaAjuste=strtotime($fecha);
+
+	echo '<div class="caja">';
+		echo '<div class="fecha">	'.date('d/m/Y',$fechaAjuste).'</div>';
+		echo '<div class="descripcion">'.$descripcion.'</div>';
+		echo '<div class="maxima">Maxima: '.$maxima.'	°F</div>';
+
+		if(!empty($minima)){
+			echo '<div class="minima">Minima: '.$minima.'	°F</div>';
+		}
+
+	echo "</div>";
+
+}
 
  ?>
